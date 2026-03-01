b63ea12277d1891f8569eca66c74e68974a0d5c1
Даниил Морозов <dan4ik082005@mail.ru>
2026-03-02 02:05:51 +0300

    Solved problem №3

---
+++ 3_Longest_String_Without_Repeating_Characters.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        last_seen = {}
+        max_len = 0
+        left = 0
+        
+        for right, char in enumerate(s):
+
+            if char in last_seen:
+                left = max(left, last_seen[char] + 1)
+            
+            last_seen[char] = right
+            
+            max_len = max(max_len, right - left + 1)
+            
+        return max_len
+    
